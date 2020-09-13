695115c35dd7963ac2cfbfa92b740bde81914f3e
Ian Lucas <ianlucas_ferreira@hotmail.com>
2020-09-13 02:08:18 -0300

    ServiceLogApp: make buttons work

--- src/ServiceLogRequest.js
+++ src/ServiceLogRequest.js
@@ -1,12 +1,70 @@
 import React from 'react'
+import escape from 'js-string-escape'
 import { ButtonGroup, Button, Callout } from '@blueprintjs/core'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { magula } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import ServiceLogInfobox from './ServiceLogInfobox'
-import { beautifier } from './utils'
+import { beautifier, copyText, copyHtmlElement, writeFile } from './utils'
 import './styles/ServiceLogRequest.css'
 
 export default function ServiceLogRequest (props) {
+  const beautyRequest = beautifier(props.type, props.request)
+  const beautyResponse = beautifier(props.type, props.response)
+  const headerText = [
+    `Interface: ${props.name}`,
+    `Endpoint: ${props.endpoint}`,
+    `Date: ${props.requestTime.dateText} ${props.requestTime.timeText}-${props.responseTime.timeText}`
+  ].join('\n')
+
+  function copyHeader () {
+    copyText(headerText)
+  }
+
+  function copyRequest () {
+    copyText(beautyRequest)
+  }
+
+  function copyResponse () {
+    copyText(beautyResponse)
+  }
+
+  function copyMessage () {
+    copyText([
+      headerText,
+      '[Request]',
+      beautyRequest,
+      '[Response]',
+      beautyResponse
+    ].join('\n'))
+  }
+
+  function copyEmail () {
+    let body = ''
+    body += `<p>${headerText.replace(/\n/g, '<br>')}</p>`
+    body += '<p>'
+    body += '<table style="border-collapse: collapse; white-space: pre; border: 1px solid #000;">'
+    body += '<tr>'
+    body += `<td style="vertical-align: top; border: 1px solid #000; padding: 4px;">${beautyRequest.replace(/\n/g, '<br>')}</td>`
+    body += `<td style="vertical-align: top; border: 1px solid #000; padding: 4px;">${beautyResponse.replace(/\n/g, '<br>')}</td>`
+    body += '</tr>'
+    body += '</table>'
+    body += '</p>'
+    copyHtmlElement(body)
+  }
+
+  function copyMock () {
+    copyText(escape(JSON.stringify(beautyResponse)))
+  }
+
+  function download () {
+    if (props.type === 'json') {
+      writeFile(`${props.name}-Request.json`, beautyRequest)
+      writeFile(`${props.name}-Response.json`, beautyResponse)
+    } else {
+      writeFile(`${props.name}.xml`, `${beautyRequest}\n${beautyResponse}`, 'text/xml')
+    }
+  }
+
   return (
     <>
       <hr />
@@ -17,23 +75,26 @@ export default function ServiceLogRequest (props) {
           <p><em>Data:</em> {props.requestTime.dateText} {props.requestTime.timeText}-{props.responseTime.timeText}</p>
         </header>
         <ButtonGroup minimal>
-          <Button icon='clipboard'>
-            Cabeçalho
+          <Button icon='clipboard' onClick={copyHeader}>
+            Header
           </Button>
-          <Button icon='clipboard'>
+          <Button icon='clipboard' onClick={copyRequest}>
             Request
           </Button>
-          <Button icon='clipboard'>
+          <Button icon='clipboard' onClick={copyResponse}>
             Response
           </Button>
-          <Button icon='clipboard'>
-            Mensagem
+          <Button icon='clipboard' onClick={copyMessage}>
+            Message
           </Button>
-          <Button icon='envelope'>
+          <Button icon='envelope' onClick={copyEmail}>
             Email
           </Button>
-          <Button icon='hat'>
+          <Button icon='hat' onClick={copyMock}>
             Mock
+          </Button>
+          <Button icon='download' onClick={download}>
+            Download
           </Button>
         </ButtonGroup>
         {props.alerts && (
@@ -54,12 +115,12 @@ export default function ServiceLogRequest (props) {
         <div className='service-log-request-payloads'>
           <div className='service-log-request-payload'>
             <div className='service-log-request-payload-code'>
-              <SyntaxHighlighter language={props.type} style={magula}>{beautifier(props.type, props.request)}</SyntaxHighlighter>
+              <SyntaxHighlighter language={props.type} style={magula}>{beautyRequest}</SyntaxHighlighter>
             </div>
           </div>
           <div className='service-log-request-payload'>
             <div className='service-log-request-payload-code'>
-              <SyntaxHighlighter language={props.type} style={magula}>{beautifier(props.type, props.response)}</SyntaxHighlighter>
+              <SyntaxHighlighter language={props.type} style={magula}>{beautyResponse}</SyntaxHighlighter>
             </div>
           </div>
         </div>
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 import beautify from 'js-beautify'
 import sortBy from 'lodash.sortby'
+import FileSaver from 'file-saver'
 
 export function createGroupsFromRows (rows) {
   const result = []
@@ -35,4 +36,54 @@ export function beautifier (type, text) {
   } else if (type === 'xml') {
     return beautify.html_beautify(text, options)
   }
+}
+
+/**
+ * @param {HTMLElement|String} htmlElement
+ */
+export function copyHtmlElement (htmlElement) {
+  let element = htmlElement
+  let dummy = null
+
+  if (typeof htmlElement === 'string') {
+    dummy = document.createElement('div')
+    dummy.innerHTML = htmlElement
+    document.body.appendChild(dummy)
+    element = dummy
+  }
+
+  if (document.body.createTextRange) {
+    const range = document.body.createTextRange()
+    range.moveToElementText(element)
+    range.select()
+    document.execCommand('Copy')
+  } else if (window.getSelection) {
+    const selection = window.getSelection()
+    const range = document.createRange()
+    range.selectNodeContents(element)
+    selection.removeAllRanges()
+    selection.addRange(range)
+    document.execCommand('Copy')
+  }
+
+  if (dummy) {
+    document.body.removeChild(dummy)
+  }
+}
+
+/**
+ * @param {String} text
+ */
+export function copyText (text) {
+  const textarea = document.createElement('textarea')
+  document.body.appendChild(textarea)
+  textarea.value = text
+  textarea.select()
+  document.execCommand('copy')
+  document.body.removeChild(textarea)
+}
+
+export function writeFile (name, content, type = 'application/json;charset=utf-8') {
+  const blob = new window.Blob([content], { type })
+  FileSaver.saveAs(blob, name)
 }

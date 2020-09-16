c994a67512156bd46e38415627bc5e2b46b954e8
Ian Lucas <ianlucas_ferreira@hotmail.com>
2020-09-16 00:19:09 -0300

    Add scripts file

---
+++ src/scripts.js
@@ -0,0 +1,10 @@
+export default {
+  termInterface: 'Inteface',
+  termEndpoint: 'Endpoint',
+  termDate: 'Date',
+  termRequest: 'Request',
+  termResponse: 'Response',
+  requestHeader: '%s: %s\n%s: %s\n%s: %s %s-%s',
+  requestMessage: '%s\n[%s]\n\n%s\n\n[%s]\n\n%s',
+  requestEmail: '<p>%s</p><p><table style="border-collapse: collapse; white-space: pre; border: 1px solid #000;"><tr><td style="vertical-align: top; border: 1px solid #000; padding: 4px;">%s</td><td style="vertical-align: top; border: 1px solid #000; padding: 4px;">%s</td></tr></table></p>'
+}

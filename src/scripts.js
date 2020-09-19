7705a6737e8d0be21f4959aa75b0dc7abc0054e0
Ian Lucas <ianlucas_ferreira@hotmail.com>
2020-09-19 13:46:57 -0300

    Update scripts

--- src/scripts.js
+++ src/scripts.js
@@ -4,7 +4,12 @@ export default {
   termDate: 'Date',
   termRequest: 'Request',
   termResponse: 'Response',
+  termHeader: 'Header',
+  termMessage: 'Message',
+  termEmail: 'E-mail',
+  termMock: 'Mock',
+  termDownload: 'Download',
   requestHeader: '%s: %s\n%s: %s\n%s: %s %s-%s',
-  requestMessage: '%s\n[%s]\n\n%s\n\n[%s]\n\n%s',
+  requestMessage: '%s\n\n[%s]\n\n%s\n\n[%s]\n\n%s',
   requestEmail: '<p>%s</p><p><table style="border-collapse: collapse; white-space: pre; border: 1px solid #000;"><tr><td style="vertical-align: top; border: 1px solid #000; padding: 4px;">%s</td><td style="vertical-align: top; border: 1px solid #000; padding: 4px;">%s</td></tr></table></p>'
 }
